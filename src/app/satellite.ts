53cf17d70a724a6278c3539daabb77eb3695e0eb
Tuma Ussiri <tussiri@gmail.com>
2022-12-12 20:03:58 -0600

    finsihed step 3

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,23 +1,27 @@
 export class Satellite {
+  name: string;
+  type: string;
+  launchDate: string;
+  orbitType: string;
+  operational: boolean;
 
-	name: string;
-	type: string;
-	launchDate: string;
-	orbitType: string;
-	operational: boolean;
+  constructor(
+    name: string,
+    type: string,
+    launchDate: string,
+    orbitType: string,
+    operational: boolean
+  ) {
+    this.name = name;
+    this.type = type;
+    this.launchDate = launchDate;
+    this.orbitType = orbitType;
+    this.operational = operational;
+  }
 
-	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
-		this.name = name;
-		this.type = type;
-		this.launchDate = launchDate;
-		this.orbitType = orbitType;
-		this.operational = operational;
-   }
-	
-	isSpaceDebris(): boolean {
-		return true;
-   }
-
+  isSpaceDebris(): boolean {
+    return this.type === "Space Debris";
+  }
 }
 
 // TODO 3a: fix isSpaceDebris check
